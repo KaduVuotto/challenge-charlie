f14151dc64b4be079fb3d3ee8396fe8728cb9e28
Kadu Vuotto <cev@lionx.com.br>
2024-05-15 10:58:42 -0300

    create functions

---
+++ weather-forecast/src/functions/weatherDescriptions.ts
@@ -0,0 +1,24 @@
+export const weatherDescriptions = (description: string) => {
+    switch (description) {
+        case "clear sky":
+            return "Céu limpo";
+        case "few clouds":
+            return "Poucas nuvens";
+        case "scattered clouds":
+            return "Nuvens dispersas";
+        case "broken clouds":
+            return "Nuvens quebradas";
+        case "shower rain":
+            return "Chuva de ducha";
+        case "rain":
+            return "Chuva";
+        case "thunderstorm":
+            return "Trovoada";
+        case "snow":
+            return "Neve";
+        case "mist":
+            return "Névoa";
+        default:
+            return description;
+    }
+};
